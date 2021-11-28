125e3ffdd78ce78423b99edb4b971909c45f67a8
Naira <nairagezhoyan@gmail.com>
2021-11-28 17:03:37 +0400

    connect with mongodb

--- src/pages/api/tasks.ts
+++ src/pages/api/tasks.ts
@@ -1,13 +1,36 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { MongoClient } from 'mongodb';
 
 type Data = {
   name: string
 }
 
-export default function handler(
+async function handlerTasks(
   req: NextApiRequest,
   res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+)
+{
+  try
+  {
+    if(req.method === 'POST') {
+    const data = req.body;
+    const client = await MongoClient.connect(process.env.NEXT_PUBLIC_MONGODB_URI || 'mongodb+srv://naira:987654321@cluster0.clov1.mongodb.net/tasks?retryWrites=true&w=majority');
+    console.log( 'MongoDB connected' );
+      
+    const db = client.db();
+
+    const tasksCollection = db.collection( 'tasks' );
+    const result = await tasksCollection.insertOne( data );
+    console.log( result );
+
+    client.close();
+
+    res.status( 200 ).json( { name: 'John Doe' } )
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+export default handlerTasks;
